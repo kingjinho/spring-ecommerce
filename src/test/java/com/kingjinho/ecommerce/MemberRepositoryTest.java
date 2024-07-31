8186e3750213576ea8c8d487bd580c493818d4fa
kingjinho <jinhoshin319@gmail.com>
2024-07-31 21:58:38 +0900

    test: Rollback 어노테이션 추가

--- src/test/java/com/kingjinho/ecommerce/MemberRepositoryTest.java
+++ src/test/java/com/kingjinho/ecommerce/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception {
         //give
         Member member = new Member();
