6d63d0c9e1670be28e1c28a28f4db9b588be65f4
kingjinho <jinhoshin319@gmail.com>
2024-11-03 16:28:23 +0900

    feat: 상품 정보 업데이트 기능 추가

--- src/main/java/com/kingjinho/ecommerce/controller/ItemController.java
+++ src/main/java/com/kingjinho/ecommerce/controller/ItemController.java
@@ -1,12 +1,12 @@
 package com.kingjinho.ecommerce.controller;
 
 import com.kingjinho.ecommerce.domain.item.Book;
+import com.kingjinho.ecommerce.domain.item.Item;
 import com.kingjinho.ecommerce.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,5 +40,36 @@ public class ItemController {
         return "items/itemList";
     }
 
-//    @GetMapping()
+    @GetMapping("/items/{id}/edit")
+    public String updateItemForm(@PathVariable("id") Long id, Model model) {
+        Book book = (Book) itemService.findOne(id);
+
+        BookForm form = new BookForm();
+        form.setId(book.getId());
+        form.setName(book.getName());
+        form.setPrice(book.getPrice());
+        form.setStockQuantity(book.getStockQuantity());
+        form.setAuthor(book.getAuthor());
+        form.setIsbn(book.getIsbn());
+
+        model.addAttribute("form", form);
+
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{id}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+        Book book = new Book();
+
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
+    }
 }
