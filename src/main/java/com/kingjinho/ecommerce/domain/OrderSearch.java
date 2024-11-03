de9de6e0e7f28ef2e3cf0ef0110e32b401d3b56c
kingjinho <jinhoshin319@gmail.com>
2024-11-03 15:51:19 +0900

    feat: 주문 내역 조회 기능 추가

---
+++ src/main/java/com/kingjinho/ecommerce/domain/OrderSearch.java
@@ -0,0 +1,12 @@
+package com.kingjinho.ecommerce.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+}
